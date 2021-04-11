960e650d65f11a780244a91a94485756e6c95ca0
ChuLeeLock <13132193357@163.com>
2021-04-11 21:54:28 +0800

    项目初始化

---
+++ src/main/java/com/y/chat/ChatApplication.java
@@ -0,0 +1,13 @@
+package com.y.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatApplication.class, args);
+    }
+
+}

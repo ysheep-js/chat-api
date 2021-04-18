c064dd384e5b85d6f24ce2ed2f161bcb42530d33
dong <donghao@fang.com>
2021-04-18 18:50:33 +0800

    运行bug

--- src/main/java/com/y/chat/ChatApplication.java
+++ src/main/java/com/y/chat/ChatApplication.java
@@ -1,9 +1,11 @@
 package com.y.chat;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.y.chat.mapper")
 public class ChatApplication {
 
     public static void main(String[] args) {

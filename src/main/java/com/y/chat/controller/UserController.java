ca74ddfe4bae91672bd3f9ad3e10051666fe6461
dong <donghao@fang.com>
2021-04-18 18:50:48 +0800

    登录+注册功能完成

--- src/main/java/com/y/chat/controller/UserController.java
+++ src/main/java/com/y/chat/controller/UserController.java
@@ -1,20 +1,73 @@
 package com.y.chat.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.y.chat.entity.User;
+import com.y.chat.pojo.bo.RegisterBo;
+import com.y.chat.service.impl.UserServiceImpl;
+import com.y.chat.tool.BaseControllerResponse;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author xxx
  * @since 2021-04-18
  */
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController {
 
+    @Autowired
+    UserServiceImpl userService;
+
+
+    @GetMapping("/login")
+    public BaseControllerResponse login(@RequestParam("username") String username,
+                                        @RequestParam("password") String password) {
+        try {
+            if (StringUtils.isBlank(username) || StringUtils.isBlank(password)) {
+                return new BaseControllerResponse().renderError("用户名和密码不能为空");
+            }
+
+            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("username", username).eq("password", password);
+            User user = userService.getBaseMapper().selectOne(queryWrapper);
+            if (user != null) {
+                return new BaseControllerResponse().renderSuccess("登录成功");
+            } else {
+                return new BaseControllerResponse().renderError("登录失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new BaseControllerResponse().renderError("登录失败");
+        }
+    }
+
+    @PostMapping("/register")
+    public BaseControllerResponse register(@RequestBody RegisterBo registerBo) {
+        try {
+            if (StringUtils.isBlank(registerBo.getUsername()) || StringUtils.isBlank(registerBo.getPassword())) {
+                return new BaseControllerResponse().renderError("用户名和密码不能为空");
+            }
+
+            User u = new User();
+            u.setUsername(registerBo.getUsername());
+            u.setPassword(registerBo.getPassword());
+            int flag = userService.getBaseMapper().insert(u);
+            if (flag != 0) {
+                return new BaseControllerResponse().renderSuccess("注册成功");
+            } else {
+                return new BaseControllerResponse().renderError("注册失败");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new BaseControllerResponse().renderError("注册失败");
+        }
+    }
 }
---
+++ src/main/java/com/y/chat/pojo/bo/RegisterBo.java
@@ -0,0 +1,16 @@
+package com.y.chat.pojo.bo;
+
+import lombok.Data;
+
+/**
+ * @Description:
+ * @Author: dh
+ * @Date: 2021/4/18 18:13
+ */
+@Data
+public class RegisterBo {
+
+    private String username;
+
+    private String password;
+}
